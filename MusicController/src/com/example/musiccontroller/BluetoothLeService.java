1c01ab042e9d004dbee330f500eb57418e6cfd4d
Mauro Del Rio <mauro@crs4.it>
2014-02-19 18:06:43 +0100

    first commit

---
+++ MusicController/src/com/example/musiccontroller/BluetoothLeService.java
@@ -0,0 +1,178 @@
+package com.example.musiccontroller;
+
+import java.util.List;
+import java.util.UUID;
+
+import android.app.Service;
+import android.bluetooth.*;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+enum SimpleKeysStatus {
+    // Warning: The order in which these are defined matters.
+    OFF_OFF, OFF_ON, ON_OFF, ON_ON;
+}
+
+public class BluetoothLeService extends Service {
+	private final static String TAG = BluetoothLeService.class.getSimpleName();
+
+    private BluetoothManager mBluetoothManager;
+    private BluetoothAdapter mBluetoothAdapter;
+    private String mBluetoothDeviceAddress;
+    private BluetoothGatt mBluetoothGatt;
+    private int mConnectionState = STATE_DISCONNECTED;
+
+    private static final int STATE_DISCONNECTED = 0;
+    private static final int STATE_CONNECTING = 1;
+    private static final int STATE_CONNECTED = 2;
+
+    public final static String ACTION_GATT_CONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
+    public final static String ACTION_GATT_DISCONNECTED =
+            "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
+    public final static String ACTION_GATT_SERVICES_DISCOVERED =
+            "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
+    public final static String ACTION_DATA_AVAILABLE =
+            "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
+    public final static String EXTRA_DATA =
+            "com.example.bluetooth.le.EXTRA_DATA";
+    
+    static final String CMDTOGGLEPAUSE = "togglepause";
+	static final String CMDPAUSE = "pause";
+	static final String CMDPREVIOUS = "previous";
+	static final String CMDNEXT = "next";
+	static final String SERVICECMD = "com.android.music.musicservicecommand";
+	static final String CMDNAME = "command";
+	static final String CMDSTOP = "stop";
+    
+    private static final UUID SIMPLE_KEYS_SERVICE_UUID = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
+    private static final UUID SIMPLE_KEYS_DATA_UUID = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
+    private BluetoothGattCharacteristic keyCharacteristic;
+    
+
+    private BluetoothDevice sensorTag;
+    
+    @Override
+    public void onStart(Intent intent, int startId) {
+    	sensorTag = intent.getExtras().getParcelable("SensorTag");
+    	Log.i(TAG, "Blconnected "+ sensorTag.getName());
+    	mBluetoothGatt = sensorTag.connectGatt(this, false, mGattCallback);
+    }
+
+    // Various callback methods defined by the BLE API.
+    private final BluetoothGattCallback mGattCallback =
+            new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status,
+                int newState) {
+            String intentAction;
+            if (newState == BluetoothProfile.STATE_CONNECTED) {
+                intentAction = ACTION_GATT_CONNECTED;
+                mConnectionState = STATE_CONNECTED;
+                broadcastUpdate(intentAction);
+                Log.i(TAG, "Connected to GATT server.");
+                Log.i(TAG, "Attempting to start service discovery:" +
+                        mBluetoothGatt.discoverServices());
+                
+            } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                intentAction = ACTION_GATT_DISCONNECTED;
+                mConnectionState = STATE_DISCONNECTED;
+                Log.i(TAG, "Disconnected from GATT server.");
+                broadcastUpdate(intentAction);
+            }
+        }
+        
+        @Override
+        public void  onCharacteristicChanged (BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            /*
+             * The key state is encoded into 1 unsigned byte.
+             * bit 0 designates the right key.
+             * bit 1 designates the left key.
+             * bit 2 designates the side key.
+             * 
+             * Weird, in the userguide left and right are opposite.
+             */
+        	Log.i(TAG, "onCharacteristicChanged");
+        	if (characteristic == keyCharacteristic){
+	            Integer encodedInteger = characteristic.getIntValue(0x11, 0);
+	        	    
+	            SimpleKeysStatus newValue = SimpleKeysStatus.values()[encodedInteger % 4];
+	            Log.i(TAG, "newValue " +  newValue.toString());
+	            if (newValue == SimpleKeysStatus.OFF_ON){
+	            	AudioManager mAudioManager = (AudioManager) getSystemService(BluetoothLeService.this.AUDIO_SERVICE);
+	            	
+    				if(mAudioManager.isMusicActive()) {
+    				    Intent i = new Intent(SERVICECMD);
+    				    i.putExtra(CMDNAME , CMDNEXT );
+    				    BluetoothLeService.this.sendBroadcast(i);
+    				}
+	        	}
+	            else if (newValue == SimpleKeysStatus.ON_OFF){
+	            	AudioManager mAudioManager = (AudioManager) getSystemService(BluetoothLeService.this.AUDIO_SERVICE);
+	            	
+    				if(mAudioManager.isMusicActive()) {
+    				    Intent i = new Intent(SERVICECMD);
+    				    i.putExtra(CMDNAME , CMDPREVIOUS);
+    				    BluetoothLeService.this.sendBroadcast(i);
+    				}
+	        	}
+        	}
+        }
+        
+        @Override
+        // New services discovered
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+            	Log.i(TAG, "onServicesDiscovered received");
+                broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
+                List<BluetoothGattService> services = gatt.getServices();
+                for (int i = 0; i < services.size(); i++){
+                	Log.i(TAG, "service " + services.get(i).getUuid());
+                }
+                keyCharacteristic = gatt.getService(SIMPLE_KEYS_SERVICE_UUID).getCharacteristic(SIMPLE_KEYS_DATA_UUID);
+                boolean notification_enabled = gatt.setCharacteristicNotification(keyCharacteristic, true);
+                Log.i(TAG, "notification_enabled: " + notification_enabled);
+                
+                BluetoothGattDescriptor descriptor = keyCharacteristic.getDescriptor(UUID.fromString("00002902-0000-1000-8000-00805f9b34fb"));
+                descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                mBluetoothGatt.writeDescriptor(descriptor);
+//                Toast.makeText(BluetoothLeService.this, "", 5000).show();
+            } else {
+                Log.w(TAG, "onServicesDiscovered received: " + status);
+            }
+        }
+        
+        @Override
+        // Result of a characteristic read operation
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                BluetoothGattCharacteristic characteristic,
+                int status) {
+        	
+        	Log.i(TAG, characteristic.getUuid().toString());
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            }
+        }
+    };
+    private void broadcastUpdate(final String action) {
+        final Intent intent = new Intent(action);
+        sendBroadcast(intent);
+    }
+    
+    private void broadcastUpdate(final String action,
+        final BluetoothGattCharacteristic characteristic) {
+		final Intent intent = new Intent(action);
+//		sendBroadcast(intent);
+		}
+
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+	
---
+++ MusicController/src/com/example/musiccontroller/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.musiccontroller;
+
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+	Button nextSong;
+	static private final String TAG = "MusicController";
+	static final String CMDTOGGLEPAUSE = "togglepause";
+	static final String CMDPAUSE = "pause";
+	static final String CMDPREVIOUS = "previous";
+	static final String CMDNEXT = "next";
+	static final String SERVICECMD = "com.android.music.musicservicecommand";
+	static final String CMDNAME = "command";
+	static final String CMDSTOP = "stop";
+	private static final int REQUEST_ENABLE_BT = 0;
+	private BluetoothAdapter mBluetoothAdapter;
+	private static final long SCAN_PERIOD = 10000;
+	private boolean mScanning;
+    private Handler mHandler;
+    private Button scanBLE;
+    
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, int rssi,
+                byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+               @Override
+               public void run() {
+            	   Log.i(TAG,"Found device " + device.getName());	
+            	   if (device.getName().contains("SensorTag") ){
+            		   Log.i(TAG,"Starting service...");
+            		   	mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            		   	Intent intent = new Intent(MainActivity.this, BluetoothLeService.class);
+	   					intent.putExtra("SensorTag",device);
+	   					startService(intent);   
+            	   }
+               }
+           });
+       }
+    };
+	
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        scanBLE = (Button) findViewById(R.id.scan);
+        scanBLE.setOnClickListener(new OnClickListener() {
+        	@Override
+    		public void onClick(View v) {
+    			scanLeDevice(true);
+    		}
+        });
+        
+        nextSong = (Button) findViewById(R.id.nextsong);
+        nextSong.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+//				Intent i = new Intent(Intent.ACTION_MEDIA_BUTTON);
+//				i.putExtra(Intent.EXTRA_KEY_EVENT,new KeyEvent(KeyEvent.ACTION_UP, KeyEvent.KEYCODE_MEDIA_NEXT));
+//				//i.putExtra(Intent.EXTRA_KEY_EVENT, KeyEvent.KEYCODE_MEDIA_NEXT);
+//				sendOrderedBroadcast(i, null);
+//				Log.i(TAG, "Send intent in broadcast");
+			
+//			AudioManager mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+//
+//			if(mAudioManager.isMusicActive()) {
+//			    Intent i = new Intent(SERVICECMD);
+//			    i.putExtra(CMDNAME , CMDNEXT );
+//			    MainActivity.this.sendBroadcast(i);
+//			}
+			
+			
+			}
+		});
+        
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+       
+        scanLeDevice(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    private void scanLeDevice(final boolean enable) {
+    	Log.i(TAG, "starting scanLeDevice");
+    	mScanning = true;
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
+    	if (enable) {
+            // Stops scanning after a pre-defined scan period.
+    		mHandler = new Handler();
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+    
+    }
+}
